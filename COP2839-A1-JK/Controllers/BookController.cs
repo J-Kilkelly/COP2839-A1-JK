409d102031e861001ab8cdc030adfec995338601
John Kilkelly <JKilkelly@live.com>
2025-08-24 21:08:01 -0400

    Second commit from CLI

---
+++ COP2839-A1-JK/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using COP2839_A1_JK.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace COP2839_A1_JK.Controllers
+{
+	public class BookController : Controller
+	{
+		public static List<Book> books = new List<Book>
+			{
+				new Book { Id = 1, Title = "Murach's C#", Author = "Boehm & Murach" },
+				new Book { Id = 2, Title = "Head First C#", Author = "Stellman & Greene" }
+			};
+
+		// GET: /Book/
+		
+		public IActionResult Index()
+		{
+			return View(books);
+		}
+
+		[HttpGet]
+		public IActionResult AddBook()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult AddBook(Book model)
+		{
+			if (ModelState.IsValid)
+			{
+				model.Id = books.Count + 1;
+				books.Add(model);
+				return RedirectToAction("Index");
+			}
+			return View(model);
+		}
+	}
+}
